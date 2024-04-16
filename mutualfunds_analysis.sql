b9e9053fde3693bd8bf92166ca02b9371517669c
Rajani Yadav <100602312+Rajaniy718@users.noreply.github.com>
2024-04-16 10:35:22 +0530

    Update mutualfunds_analysis.sql

--- mutualfunds_analysis.sql
+++ mutualfunds_analysis.sql
@@ -1,6 +1,3 @@
-select *
-from [dbo].[comprehensive_mutual_funds_data]
-
 SELECT *
 FROM [dbo].[comprehensive_mutual_funds_data]
 WHERE COALESCE(scheme_name, min_sip, min_lumpsum, expense_ratio,fund_size_cr, fund_age_yr, fund_manager, alpha, sd, beta, sharpe, risk_level,
@@ -8,11 +5,12 @@ WHERE COALESCE(scheme_name, min_sip, min_lumpsum, expense_ratio,fund_size_cr, fu
 
 SELECT returns_3yr
 FROM [dbo].[comprehensive_mutual_funds_data]
-
+	
+--------- Handling missing numeric values separately------------ 	
 SELECT *
 FROM [dbo].[comprehensive_mutual_funds_data]
-WHERE returns_3yr IS NULL OR returns_3yr = '' -- Handle missing numeric values separately
-  
+WHERE returns_3yr IS NULL OR returns_3yr = '' 
+	
 SELECT *
 FROM [dbo].[comprehensive_mutual_funds_data]
 WHERE alpha IS NULL OR alpha = '' 
@@ -21,15 +19,11 @@ SELECT *
 FROM [dbo].[comprehensive_mutual_funds_data]
 WHERE category IS NULL OR category = ''
 
-
 SELECT *
 FROM [dbo].[comprehensive_mutual_funds_data]
 WHERE category IS NULL OR category = '' OR min_lumpsum IS NULL OR min_lumpsum ='';
 
-
-
---missing values-- category ,sub_category ,amc_name , scheme_name, fund_manager, returns_3yr, returns_5yr, min_sip , 
-       min_lumpsum, expense_ratio, fund_size_cr, fund_age_yr, alpha, sd, beta, sharpe, risk_level, rating,returns_1yr
+------- Handling missing values togerther---
 
 SELECT *
 FROM [comprehensive_mutual_funds_data]
@@ -46,12 +40,12 @@ where min_sip = '' OR min_lumpsum= '' OR expense_ratio= '' OR fund_size_cr= '' O
 returns_1yr = '' ;
 ;
 
-
 SELECT category ,sub_category ,amc_name , scheme_name, fund_manager, returns_3yr, returns_5yr, min_sip , 
        min_lumpsum, expense_ratio, fund_size_cr, fund_age_yr, alpha, sd, beta, sharpe, risk_level, rating,returns_1yr
 FROM [comprehensive_mutual_funds_data]
 WHERE returns_3yr = '' OR returns_5yr = '' OR category = '' OR sub_category='' OR amc_name= '' OR scheme_name = '' OR fund_manager='' ;
 
+--------Checking numeric values----------
 
 SELECT category, sub_category, amc_name, scheme_name, fund_manager, returns_3yr, returns_5yr, min_sip,
        min_lumpsum, expense_ratio, fund_size_cr, fund_age_yr, alpha, sd, beta, sharpe, risk_level, rating, returns_1yr
@@ -66,7 +60,7 @@ WHERE (ISNUMERIC(returns_3yr) = 1 AND ISNUMERIC(returns_5yr) = 1)
    OR category = '' OR sub_category = '' OR amc_name = '' OR scheme_name = '' OR fund_manager = '';
 
 
---Duplicate values--
+--------Checking Duplicate values---------
 
 SELECT category ,sub_category ,amc_name , scheme_name, fund_manager, returns_3yr, returns_5yr, min_sip , 
        min_lumpsum, expense_ratio, fund_size_cr, fund_age_yr, alpha, sd, beta, sharpe, risk_level, rating,returns_1yr ,COUNT(*) AS duplicate_count 
@@ -85,21 +79,10 @@ HAVING COUNT(*)> 1
 
 ) AS duplicates;
 
-
 SELECT category, sub_category, amc_name, scheme_name, fund_manager,returns_3yr, returns_5yr,  COUNT(*) AS duplicate_count
     FROM [dbo].[comprehensive_mutual_funds_data]
     GROUP BY category, sub_category, amc_name, scheme_name, fund_manager, returns_3yr, returns_5yr
     HAVING COUNT(*) > 1
-
-
-SELECT *
-From [dbo].[comprehensive_mutual_funds_data]
-WHERE category = 'Hybrid' AND amc_name = 'JM Financial Mutual Fund'
-
-
-SELECT scheme_name, min_sip , min_lumpsum, expense_ratio, fund_size_cr, fund_age_yr, alpha, sd, beta, sharpe, risk_level, rating,returns_1yr, returns_3yr, returns_5yr
-FROM [dbo].[comprehensive_mutual_funds_data]
-
 
 SELECT SUM(duplicate_count) AS total_duplicate_count
 FROM (
@@ -113,15 +96,13 @@ select *
 from [dbo].[comprehensive_mutual_funds_data]
 where amc_name = 'Nippon India Mutual Fund' and fund_manager = 'Vivek Sharma'
 
-
------------summary statistics 
+-----------Summary Statistics ----------------
 ---Categorical analysis: 
 ---1. Scheme_name
 
 SELECT DISTINCT(scheme_name) , count(*) as total_count
 FROM [dbo].[comprehensive_mutual]_funds_data]
 GROUP BY (scheme_name)
-
 ORDER BY DESC
 
 SELECT DISTINCT(sum(scheme_name))
@@ -141,12 +122,11 @@ from (
 Group by scheme_name, total_count
 ORDER BY total_count DESC
 
-
 select *
 from [dbo].[comprehensive_mutual_funds_data]
 where  scheme_name = 'SBI Long Term Advantage Fund'
 
-----Fund_manager - fund manager with top 5 fund size and rating -???
+------ Fund_manager - fund manager with top 5 fund size and rating --------???
 
 SELECT TOP 5 (fund_manager), fund_size_cr, rating, count(*) as total_count
 FROM [dbo].[comprehensive_mutual_funds_data]
@@ -158,22 +138,22 @@ FROM [dbo].[comprehensive_mutual_funds_data]
 GROUP BY fund_manager,fund_size_cr, rating
 ORDER BY rating DESC , fund_size_cr DESC
 
+------- Asset Management Companies (AMCs) -- AMC market share: top three amc's -----------
 
-
-------- Asset Management Companies (AMCs)
-
-SELECT DISTINCT(amc_name) , count(amc_name) as 'total_count'  --------AMC market share: top three amc's
+SELECT DISTINCT(amc_name) , count(amc_name) as 'total_count'  
 FROM [dbo].[comprehensive_mutual_funds_data]
 GROUP BY amc_name
 ORDER BY 2 DESC
 
-SELECT TOP 3 amc_name , count(amc_name) as'total_count' ---- top 3 with values
+---- top 3 with values-----------
+
+SELECT TOP 3 amc_name , count(amc_name) as'total_count' 
 FROM [dbo].[comprehensive_mutual_funds_data]
 GROUP BY amc_name
 ORDER BY COUNT(*) DESC;
 
----------AMC performance: Analyze whether funds from specific AMCs tend to have higher or lower ratings, returns, or other characteristics. 
-----------AMC reputation: Identify well-known AMCs in your dataset.
+---------AMC performance: Analyze whether funds from specific AMCs tend to have higher or lower ratings, returns, or other characteristics. -------------
+----------AMC reputation: Identify well-known AMCs in your dataset ------------
 
 SELECT  amc_name, returns_1yr, returns_3yr, returns_5yr
 FROM [dbo].[comprehensive_mutual_funds_data]
@@ -184,7 +164,6 @@ SELECT  Distinct amc_name, rating --, returns_1yr, returns_3yr, returns_5yr
 FROM [dbo].[comprehensive_mutual_funds_data]
 WHERE rating = 5
 GROUP BY amc_name, rating --, returns_1yr, returns_3yr, returns_5yr
-
 
 SELECT  TOP 3 amc_name,  returns_1yr, returns_3yr, returns_5yr
 FROM [dbo].[comprehensive_mutual_funds_data]
@@ -203,10 +182,8 @@ WHERE rating = 5 and amc_name ='Bank of India Mutual Fund'
 --GROUP BY amc_name, returns_1yr 
 --ORDER BY returns_1yr ASC
 
-
 SELECT DISTINCT amc_name
 FROM [dbo].[comprehensive_mutual_funds_data]
-
 
 -------------------Return Analysis--------------
 -- Data Preparation---
@@ -214,11 +191,11 @@ FROM [dbo].[comprehensive_mutual_funds_data]
 
 --Mean--
 SELECT AVG(column_name) AS mean_value
-FROM your_table;
+FROM [dbo].[comprehensive_mutual_funds_data];
 
 --Median--
 SELECT column_name AS median_value
-FROM your_table
+FROM [dbo].[comprehensive_mutual_funds_data]
 ORDER BY column_name
 LIMIT 1 OFFSET (SELECT COUNT(*) FROM your_table) / 2;
 
@@ -226,7 +203,7 @@ LIMIT 1 OFFSET (SELECT COUNT(*) FROM your_table) / 2;
 WITH RankedValues AS (
   SELECT column_name,
          ROW_NUMBER() OVER (PARTITION BY column_name ORDER BY COUNT(*) DESC) AS rnk
-  FROM your_table
+  FROM [dbo].[comprehensive_mutual_funds_data]
   GROUP BY column_name
 )
 SELECT column_name AS mode_value
@@ -234,24 +211,21 @@ FROM RankedValues
 WHERE rnk = 1;
 
 --Standard Deviation--
-
 SELECT STDDEV(column_name) AS std_dev_value
-FROM your_table;
-
+FROM [dbo].[comprehensive_mutual_funds_data];
 
 --Min and Max--
 SELECT MIN(column_name) AS min_value,
        MAX(column_name) AS max_value
-FROM your_table;
+FROM [dbo].[comprehensive_mutual_funds_data];
 
 --Count--
 SELECT COUNT(*) AS row_count
-FROM your_table;
+FROM [dbo].[comprehensive_mutual_funds_data];
 
 --Quartile--
 SELECT percentile_cont(0.25) WITHIN GROUP (ORDER BY column_name) AS q1_value
-FROM your_table;
-
+FROM [dbo].[comprehensive_mutual_funds_data];
 
 
 ------Time Series Analysis---------
